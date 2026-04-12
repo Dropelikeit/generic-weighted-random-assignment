9df5bf25c4f31fc3dc8d010940af8095c219af9d
Marcel Strahl <info@marcel-strahl.de>
2026-04-12 13:43:38 +0200

    Update logging initialization to use stderr as writer

--- src/infra/logging.rs
+++ src/infra/logging.rs
@@ -9,5 +9,9 @@ use tracing_subscriber::{fmt, EnvFilter};
 pub fn init() {
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
-    fmt().with_env_filter(filter).with_target(true).init();
+    fmt()
+        .with_writer(std::io::stderr)
+        .with_env_filter(filter)
+        .with_target(true)
+        .init();
 }
